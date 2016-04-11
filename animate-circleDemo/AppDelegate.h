cafce4bf550947ebbd83c55b52f62c141997197f
huangshaohua <651024752@qq.com>
2016-04-11 16:41:19 +0800

    Initial Commit

---
+++ animate-circleDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  animate-circleDemo
+//
+//  Created by 黄少华 on 16/4/11.
+//  Copyright © 2016年 黄少华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
